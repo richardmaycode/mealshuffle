e1f462005a738f3b8fb0ecd2f6231bef6c626701
Richard Wise <richard.wise@hey.com>
2025-08-30 00:00:54 +0200

    Add Stimulus controller for dropdown

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,10 @@
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
+import { Application } from "@hotwired/stimulus";
+import Dropdown from "@stimulus-components/dropdown";
+const application = Application.start();
+application.register("dropdown", Dropdown);
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
